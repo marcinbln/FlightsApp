483bdf0dd34245b5ad6dd4e24bd2f7f05cb90563
balanaQTF <marcinbalana@gmail.com>
2023-12-30 18:00:23 +0100

    formatting

--- app/src/main/java/com/example/flightsapp/core/model/Airport.kt
+++ app/src/main/java/com/example/flightsapp/core/model/Airport.kt
@@ -1,8 +1,6 @@
 package com.example.flightsapp.core.model
 
-@Suppress(
-    "SpellCheckingInspection"
-)
+@Suppress("SpellCheckingInspection")
 data class Airport(
     val iata: String = "",
     val cityName: String = "",
